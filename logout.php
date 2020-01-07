2e580ebd53d42498fe1fc33081315055300d364d
beingbhaijaan <42483778+beingbhaijaan@users.noreply.github.com>
2020-01-08 00:42:11 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_destroy();
-unset($_SESSION['id']);
-unset($_SESSION['email']);
-unset($_SESSION['verify']);
+// unset($_SESSION['id']);
+// unset($_SESSION['email']);
+// unset($_SESSION['verify']);
 ?>
